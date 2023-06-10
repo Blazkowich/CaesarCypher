fb308e69f194ccfe93075d253a50632e77ef89a2
Blazkowich <oiluridze@gmail.com>
2023-06-10 21:04:58 +0400

    Add project files.

---
+++ caesarcypher/Display.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using encryption;
+using decryption;
+
+namespace displayInputOutput
+{
+    class Display
+    {
+        public static void DisplayEncrypted(string plaintxt, int shift)
+        {
+            Console.WriteLine("| Encryption |\n------------------------\nPlain Text: ");
+            foreach (var item in plaintxt)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine("\n------------------------\nCipher Text: ");
+
+            string encryptedText = Encryption.Encrypt(plaintxt, shift);
+            Console.WriteLine(encryptedText);
+            Console.WriteLine("------------------------\n| Encryption Is Done. |\n");
+        }
+
+
+        public static void DisplayDecrypted(string cipherText, int shift)
+        {
+            Console.WriteLine("| Decryption |\n------------------------\nPlain Text: ");
+            foreach (var item in cipherText)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine("\n------------------------\nCipher Text: ");
+            var decryptedText = Decryption.Decrypt(cipherText, shift);
+            Console.WriteLine(decryptedText);
+            Console.WriteLine("------------------------\n| Decryption Is Done. |");
+        }
+    }
+}
---
+++ caesarcypher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+using decryption;
+using encryption;
+using displayInputOutput;
+
+namespace caesarcypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "Attack at the night to bitches";
+            string cipherText = "dwwdfn dw wkh qljkw wr elwfkhv";
+            int uniNumber = 3;
+
+            Display.DisplayEncrypted(plainText, uniNumber);
+            Display.DisplayDecrypted(cipherText, uniNumber);
+
+        }
+    }
+}
